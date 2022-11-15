95fdb929e856ad9d8ddbc28ec1d8edf7542f086a
uncleboss12 <justineobiazi@gmail.com>
2022-11-15 15:16:27 -0800

    new commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=justine
+export POSTGRES_PASSWORD=admin12345
+export POSTGRES_HOST=database-instance1.ca7ne4ilbpjk.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=udagram-db
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
-export URL=http://localhost:8100
+export URL=http://127.0.0.1:8100
